11dc6815147c670a96b9fec31d547bee7829295a
hadi7khan <hadikhancr7@gmail.com>
2023-12-25 14:36:58 +0530

    users position changed n bug fixed

--- lib/src/feature/progressline/presentation/view/widgets/viewed_by_widget.dart
+++ lib/src/feature/progressline/presentation/view/widgets/viewed_by_widget.dart
@@ -16,68 +16,68 @@ class _ViewedByWidgetState extends State<ViewedByWidget> {
   @override
   Widget build(BuildContext context) {
     print(widget.data.toString());
-    return Wrap(
-      children: [
-        Container(
-          padding: EdgeInsets.only(top: 28.h, left: 20.w, right: 20.w),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16.r),
-                topRight: Radius.circular(16.r)),
-            color: Colors.white,
-          ),
-          // height: MediaQuery.of(context).size.height * 0.6,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      widget.showText,
+    return Container(
+      padding: EdgeInsets.only(top: 28.h, left: 20.w, right: 20.w),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16.r),
+            topRight: Radius.circular(16.r)),
+        color: Colors.white,
+      ),
+      height: MediaQuery.of(context).size.height * 0.7,
+      width: MediaQuery.of(context).size.width,
+      child: SingleChildScrollView(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    widget.showText,
+                    style: TextStyle(
+                  letterSpacing: -0.3,
+                        color: Helper.baseBlack,
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: widget.data.length,
+                physics: NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    horizontalTitleGap: 12.w,
+                    dense: true,
+                    visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                    contentPadding: EdgeInsets.only(bottom: 10.h),
+                    leading: AvatarWidget(
+                      dpUrl: widget.data[index].dp != null
+                          ? widget.data[index].dpUrl!
+                          : "",
+                      name: widget.data[index].name!,
+                      backgroundColor: widget.data[index].preset!.color!,
+                      size: 32,
+                    ),
+                    title: Text(
+                      widget.data[index].name!,
                       style: TextStyle(
-                    letterSpacing: -0.3,
+                  letterSpacing: -0.3,
                           color: Helper.baseBlack,
-                          fontSize: 18.sp,
+                          fontSize: 14.sp,
                           fontWeight: FontWeight.w500),
                     ),
-                  ],
-                ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: widget.data.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      horizontalTitleGap: 12.w,
-                      dense: true,
-                      visualDensity: VisualDensity(horizontal: 0, vertical: -4),
-                      contentPadding: EdgeInsets.only(bottom: 10.h),
-                      leading: AvatarWidget(
-                        dpUrl: widget.data[index].dp != null
-                            ? widget.data[index].dpUrl!
-                            : "",
-                        name: widget.data[index].name!,
-                        backgroundColor: widget.data[index].preset!.color!,
-                        size: 32,
-                      ),
-                      title: Text(
-                        widget.data[index].name!,
-                        style: TextStyle(
-                    letterSpacing: -0.3,
-                            color: Helper.baseBlack,
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w500),
-                      ),
-                    );
-                    ;
-                  },
-                ),
-              ]),
-        ),
-      ],
+                  );
+                  ;
+                },
+              ),
+            ]),
+      ),
     );
   }
 }
