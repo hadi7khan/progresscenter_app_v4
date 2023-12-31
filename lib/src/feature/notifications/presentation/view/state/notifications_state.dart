cb3983135afc1ec339c62d731c8fb7eb31609fc2
Hadi Khan <hadikhan@Hadis-MacBook-Air.local>
2023-12-31 22:50:42 +0530

    state added

---
+++ lib/src/feature/notifications/presentation/view/state/notifications_state.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:progresscenter_app_v4/src/feature/notifications/data/models/notifications_model.dart';
+
+part 'notifications_state.freezed.dart';
+
+@freezed
+class NotificationsState with _$NotificationsState {
+  const factory NotificationsState(
+      {@Default(false) bool isFetching,
+      @Default(AsyncValue.loading())
+      AsyncValue<List<NotificationsModel>> notifications,
+      String? errorMessage}) = _NotificationsState;
+}
