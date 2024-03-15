bd942b16d85aad26c4d67351d882c5deb67058ee
Hadi Khan <hadikhan@192.168.1.15>
2024-03-15 11:57:31 +0530

    progressline filtering done

--- lib/src/feature/progressline/presentation/view/widgets/projects_filter_widget.dart
+++ lib/src/feature/progressline/presentation/view/widgets/projects_filter_widget.dart
@@ -9,8 +9,12 @@ import 'package:progresscenter_app_v4/src/feature/progressline/data/model/progre
 class ProjectsFilterWidget extends StatefulWidget {
   final List<ProgresslineProjectModel> progresslineProjects;
   final Function onChange;
+  final int postsCount;
   const ProjectsFilterWidget(
-      {super.key, required this.progresslineProjects, required this.onChange});
+      {super.key,
+      required this.progresslineProjects,
+      required this.onChange,
+      required this.postsCount});
 
   @override
   State<ProjectsFilterWidget> createState() => _ProjectsFilterWidgetState();
@@ -28,74 +32,117 @@ class _ProjectsFilterWidgetState extends State<ProjectsFilterWidget> {
       ),
       height: MediaQuery.of(context).size.height * 0.6,
       width: MediaQuery.of(context).size.width,
-      child: ListView.separated(
-          itemBuilder: (context, index) {
-            log(widget.progresslineProjects[index].id.toString());
-            return InkWell(
-              onTap: () {
-                widget.onChange(widget.progresslineProjects[index].id);
-              },
-              child: ListTile(
-                horizontalTitleGap: 8.w,
-                dense: true,
-                visualDensity: VisualDensity(horizontal: 0, vertical: 0),
-                contentPadding: EdgeInsets.zero,
-                leading: widget.progresslineProjects[index].coverImageUrl !=
-                        null
-                    ? ClipRRect(
-                        borderRadius: BorderRadius.circular(4.r),
-                        child: AspectRatio(
-                          aspectRatio: 16 / 9,
-                          child: Image.network(
-                            widget.progresslineProjects[index].coverImageUrl,
-                            fit: BoxFit.fill,
-                            gaplessPlayback: true,
-                            errorBuilder: (BuildContext context,
-                                Object exception, StackTrace? stackTrace) {
-                              return ClipRRect(
-                                child: Image.asset(
-                                  'assets/images/error_image.jpeg',
-                                  fit: BoxFit.cover,
-                                ),
-                              );
-                            },
-                          ),
-                        ))
-                    : ClipRRect(
-                        borderRadius: BorderRadius.circular(4.r),
-                        child: AspectRatio(
-                          aspectRatio: 16 / 9,
-                          child: Image.asset(
-                            'assets/images/error_image.jpeg',
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
-                title: Text(
-                  widget.progresslineProjects[index].name,
-                  style: TextStyle(
-                      letterSpacing: -0.3,
-                      color: Helper.baseBlack,
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500),
-                ),
-                subtitle: Text(
-                  widget.progresslineProjects[index].postCount.toString(),
-                  style: TextStyle(
-                      letterSpacing: -0.3,
-                      color: Helper.baseBlack.withOpacity(0.5),
-                      fontSize: 12.sp,
-                      fontWeight: FontWeight.w400),
-                ),
+      child: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              widget.onChange("All");
+            },
+            child: ListTile(
+              horizontalTitleGap: 8.w,
+              dense: true,
+              visualDensity: VisualDensity(horizontal: 0, vertical: 0),
+              contentPadding: EdgeInsets.zero,
+              leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(4.r),
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Container(color: Helper.widgetBackground),
+                  )),
+              title: Text(
+                "All Posts",
+                style: TextStyle(
+                    letterSpacing: -0.3,
+                    color: Helper.baseBlack,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w500),
               ),
-            );
-          },
-          separatorBuilder: ((context, index) {
-            return SizedBox(
-              height: 10.h,
-            );
-          }),
-          itemCount: widget.progresslineProjects.length),
+              subtitle: Text(
+                widget.postsCount.toString(),
+                style: TextStyle(
+                    letterSpacing: -0.3,
+                    color: Helper.baseBlack.withOpacity(0.5),
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          ListView.separated(
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                log(widget.progresslineProjects[index].id.toString());
+                return InkWell(
+                  onTap: () {
+                    widget.onChange(widget.progresslineProjects[index].id);
+                  },
+                  child: ListTile(
+                    horizontalTitleGap: 8.w,
+                    dense: true,
+                    visualDensity: VisualDensity(horizontal: 0, vertical: 0),
+                    contentPadding: EdgeInsets.zero,
+                    leading:
+                        widget.progresslineProjects[index].coverImageUrl != null
+                            ? ClipRRect(
+                                borderRadius: BorderRadius.circular(4.r),
+                                child: AspectRatio(
+                                  aspectRatio: 16 / 9,
+                                  child: Image.network(
+                                    widget.progresslineProjects[index]
+                                        .coverImageUrl,
+                                    fit: BoxFit.fill,
+                                    gaplessPlayback: true,
+                                    errorBuilder: (BuildContext context,
+                                        Object exception,
+                                        StackTrace? stackTrace) {
+                                      return ClipRRect(
+                                        child: Image.asset(
+                                          'assets/images/error_image.jpeg',
+                                          fit: BoxFit.cover,
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ))
+                            : ClipRRect(
+                                borderRadius: BorderRadius.circular(4.r),
+                                child: AspectRatio(
+                                  aspectRatio: 16 / 9,
+                                  child: Image.asset(
+                                    'assets/images/error_image.jpeg',
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                    title: Text(
+                      widget.progresslineProjects[index].name,
+                      style: TextStyle(
+                          letterSpacing: -0.3,
+                          color: Helper.baseBlack,
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    subtitle: Text(
+                      widget.progresslineProjects[index].postCount.toString(),
+                      style: TextStyle(
+                          letterSpacing: -0.3,
+                          color: Helper.baseBlack.withOpacity(0.5),
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: ((context, index) {
+                return SizedBox(
+                  height: 10.h,
+                );
+              }),
+              itemCount: widget.progresslineProjects.length),
+        ],
+      ),
     );
   }
 }
